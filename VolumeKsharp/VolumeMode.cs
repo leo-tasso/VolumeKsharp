b5ede6629f716607fdcef97385a1235db70b55ca
Tassinari Leonardo <leonardo.tassinari6@studio.unibo.it>
2023-07-03 13:16:37 +0200

    Fix stopwatch overflow

--- VolumeKsharp/VolumeMode.cs
+++ VolumeKsharp/VolumeMode.cs
@@ -11,6 +11,7 @@ using System.Diagnostics;
 /// <summary/> mode to show volume status on the ring.
 public class VolumeMode : IMode
 {
+    private const int ShowTime = 500;
     private const double ChangeRate = 1;
     private const float StepSize = 2;
     private const double Tolerance = 1;
@@ -20,8 +21,6 @@ public class VolumeMode : IMode
     private readonly Volume volume = new();
     private double volumeShown;
     private bool on;
-    private long swoMuted;
-    private long swo;
     private bool muted;
     private bool mutedOld;
 
@@ -37,7 +36,7 @@ public class VolumeMode : IMode
     /// <inheritdoc/>
     public bool IncomingCommands(InputCommands command)
     {
-        this.swo = this.sw.ElapsedMilliseconds;
+        this.sw.Restart();
         this.mutedOld = this.muted;
         switch (command)
         {
@@ -52,7 +51,7 @@ public class VolumeMode : IMode
             case InputCommands.Press:
                 this.muted = !this.muted;
                 this.volume.Muted = this.muted;
-                this.swoMuted = this.swMuted.ElapsedMilliseconds;
+                this.swMuted.Restart();
                 break;
 
             case InputCommands.Release:
@@ -62,7 +61,7 @@ public class VolumeMode : IMode
                 throw new ArgumentOutOfRangeException();
         }
 
-        if (this.swMuted.ElapsedMilliseconds > this.swoMuted + 500)
+        if (this.swMuted.ElapsedMilliseconds > ShowTime)
         {
             this.ShowVolume();
         }
@@ -73,7 +72,19 @@ public class VolumeMode : IMode
     /// <inheritdoc/>
     public void Compute()
     {
-        if (this.swMuted.ElapsedMilliseconds > this.swoMuted + 500)
+        // Stops clocks to avoid overflows.
+        if (this.swMuted is { ElapsedMilliseconds: > ShowTime, IsRunning: true })
+        {
+            this.swMuted.Stop();
+        }
+
+        if (this.sw is { ElapsedMilliseconds: > ShowTime, IsRunning: true })
+        {
+            this.sw.Stop();
+        }
+
+        // If not showing muted, show volume status.
+        if (this.swMuted.ElapsedMilliseconds > ShowTime)
         {
             if (Math.Abs(this.volume.GetVolume() - this.volumeShown) > Tolerance)
             {
@@ -81,6 +92,7 @@ public class VolumeMode : IMode
             }
         }
 
+        // Show mute status change.
         if (this.mutedOld != this.muted)
         {
             if (this.muted)
@@ -94,7 +106,8 @@ public class VolumeMode : IMode
             }
         }
 
-        if (this.sw.ElapsedMilliseconds - this.swo > 500 && this.on)
+        // Turn off after having shown the volume.
+        if (this.sw.ElapsedMilliseconds > ShowTime && this.on)
         {
             this.serialcom.AddCommand(new PercentageAppearanceCommand(Convert.ToInt32(0)));
             this.on = false;
@@ -103,7 +116,7 @@ public class VolumeMode : IMode
 
     private void ShowVolume()
     {
-        this.swo = this.sw.ElapsedMilliseconds;
+        this.sw.Restart();
         if (this.volumeShown < this.volume.GetVolume())
         {
             this.volumeShown += ChangeRate;
