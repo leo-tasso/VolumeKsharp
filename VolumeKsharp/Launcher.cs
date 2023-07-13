7fd9c55e181fa62da532e84fc71f22a45bf05656
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2023-07-13 12:28:07 +0200

    Fix windows console window appearing

--- VolumeKsharp/Launcher.cs
+++ VolumeKsharp/Launcher.cs
@@ -6,36 +6,20 @@
 namespace VolumeKsharp;
 
 using System;
-using System.Runtime.InteropServices;
 
 /// <summary>
 /// Launcher class that contains the entry point.
 /// </summary>
 public static class Launcher
 {
-    private const int Swhide = 0;
-
     /// <summary>
     /// The entry point.
     /// </summary>
     [STAThread]
     public static void Main()
  {
-     // Hide the console window
-     IntPtr hWndConsole = GetConsoleWindow();
-     if (hWndConsole != IntPtr.Zero)
-     {
-         ShowWindow(hWndConsole, Swhide);
-     }
-
      TrayIconMenu program = new TrayIconMenu();
      var controller = new Controller();
      program.ContextMenuThread(controller);
  }
-
-    [DllImport("kernel32.dll")]
-    private static extern IntPtr GetConsoleWindow();
-
-    [DllImport("user32.dll")]
-    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 }
