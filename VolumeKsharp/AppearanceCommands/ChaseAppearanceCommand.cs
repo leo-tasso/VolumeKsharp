e5bc8440a888e9cd8af87e462699ac165b58312b
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2023-07-13 21:33:16 +0200

    Add chase effect

---
+++ VolumeKsharp/AppearanceCommands/ChaseAppearanceCommand.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="ChaseAppearanceCommand.cs" company="LeonardoTassinari">
+// Copyright (c) LeonardoTassinari. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace VolumeKsharp.AppearanceCommands;
+
+/// <summary>
+/// Appearance command to set the light in the chase effect mode.
+/// </summary>
+public class ChaseAppearanceCommand : IAppearanceCommand
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChaseAppearanceCommand"/> class.
+    /// </summary>
+    /// <param name="r">Red value.</param>
+    /// <param name="g">Green value.</param>
+    /// <param name="b">Blue value.</param>
+    /// <param name="w">White value.</param>
+    /// <param name="brightness">Brightness value.</param>
+    /// <param name="speed">Speed value.</param>
+    public ChaseAppearanceCommand(int r, int g, int b, int w, int brightness, int speed)
+    {
+        this.Message = "c" + r.ToString().PadLeft(3, '0') + "," + g.ToString().PadLeft(3, '0') + "," + b.ToString().PadLeft(3, '0') + "," + w.ToString().PadLeft(3, '0') + "," + brightness.ToString().PadLeft(3, '0') + "," + speed.ToString().PadLeft(3, '0');
+    }
+
+    /// <inheritdoc />
+    public string? Message { get; }
+}
